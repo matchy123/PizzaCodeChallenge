44fdcfc65b9d1619bd71d6b6de3fe97af6dcaf2e
matchy123 <user.email>
2024-02-15 18:41:46 +0300

    Done

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <section>
+        <h1>Welcome to Diko's PIZZA PLACE</h1>
+        <p>Enjoy the Pizza ever!</p>
+      </section>
+
+      <section>
+        <h2>About Us</h2>
+        <p>
+        Diko's PIZZA PLACE has been serving delicious and freshly baked pizzas since 2020. Our passion for quality ingredients and traditional recipes sets us apart, making every bite a memorable experience.
+        </p>
+      </section>
+
+      <section>
+        <h2>Our Menu</h2>
+        <p>Explore our diverse menu featuring a variety of mouthwatering pizzas, flavorful toppings, and tempting sides. Each dish is crafted with care to satisfy your cravings.</p>
+      </section>
+
+      <section>
+        <h2>Visit Us</h2>
+        <p>
+          Join us at Juja Centre and indulge in a cozy ambiance paired with the aroma of freshly baked pizzas. Whether it's a casual family dinner, a friendly get-together, or a solo pizza adventure, we have the perfect setting for every occasion.
+        </p>
+      </section>
+
+      <section>
+        <h2>Contact Us</h2>
+        <p>For inquiries, reservations, or to place an order, feel free to contact us at 089076453. We look forward to serving you the best pizza in town!</p>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
